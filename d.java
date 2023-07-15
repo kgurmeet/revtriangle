16feac2fe90c663512b22c79caeb1798cdd3b66d
kgurmeet <131999481+kgurmeet@users.noreply.github.com>
2023-07-16 00:16:05 +0530

    Add files via upload

---
+++ d.java
@@ -0,0 +1,21 @@
+class t 
+{
+   public static void main(String args[])
+   {
+    int i=1,j=1;
+    for(  i=5;i>=1;i--)
+    {
+        for( j=1;j<=i;j++)
+        {
+            System.out.print("*");   
+        }
+        
+            
+                System.out.println();   
+            }
+            
+        
+        
+    }
+}
+
